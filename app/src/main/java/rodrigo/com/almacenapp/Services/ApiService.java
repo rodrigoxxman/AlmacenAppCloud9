df68fb3d941eeb108578859ce9f150a0553254cb
Linder Hassinger <linderhassinger00@gmail.com>
2018-05-08 09:50:27 -0500

    commit 8/5

--- app/src/main/java/rodrigo/com/almacenapp/Services/ApiService.java
+++ app/src/main/java/rodrigo/com/almacenapp/Services/ApiService.java
@@ -2,9 +2,17 @@ package rodrigo.com.almacenapp.Services;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import rodrigo.com.almacenapp.Models.Producto;
+import rodrigo.com.almacenapp.ResponseMessage;
 
 /**
  * Created by Alumno on 4/05/2018.
@@ -16,6 +24,21 @@ public interface ApiService {
 
     @GET("api/v1/productos")
     Call<List<Producto>> getProductos();
+
+    @FormUrlEncoded
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProducto(@Field("nombre") String nombre,
+                                         @Field("precio") String precio,
+                                         @Field("detalles") String detalles);
+    @Multipart
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProductoWithImage(
+            @Part("nombre") RequestBody nombre,
+            @Part("precio") RequestBody precio,
+            @Part("detalles") RequestBody detalles,
+            @Part MultipartBody.Part imagen
+    );
+
 
 }
 
